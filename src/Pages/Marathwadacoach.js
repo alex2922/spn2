1807e3ffbbdbd27a9aafd217bddf0560676e022b
Harsh Singh <alexlin2922@gmail.com>
2024-03-22 10:34:56 +0530

    update

--- src/Pages/Marathwadacoach.js
+++ src/Pages/Marathwadacoach.js
@@ -4,6 +4,21 @@ import "../Styles/marathwadacoach.scss";
 function Marathwadacoach() {
   return (
     <>
+
+    <div className="ss parent">
+
+      <div className="overlay">
+        
+      </div>
+
+      <div className="ss-cont cont">
+
+        <div className="ss-left"></div>
+        <div className="ss-right"></div>
+
+      </div>
+
+    </div>
     
     
     
