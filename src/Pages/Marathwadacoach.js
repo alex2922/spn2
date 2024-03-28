ee170d2f702df0ac70a4ce7f5485818ab7ff9bac
candyjo-72 <78309347+candyjo-72@users.noreply.github.com>
2024-03-28 16:47:06 +0530

    28/03/24 - 4:46
    changes

--- src/Pages/Marathwadacoach.js
+++ src/Pages/Marathwadacoach.js
@@ -59,43 +59,11 @@ function Marathwadacoach(props) {
               </div>
             </div>
 
-            <Counter
-              data={props.data}
-              background=""
-              textnum1="600"
-              plus="+"
-              text1={data.Marathwada_countup1}
-              textnum2="300"
-              plus2="+"
-              text2={data.Marathwada_countup2}
-              textnum3="45000"
-              plus3=""
-              text3={data.Marathwada_countup3}
-              textnum4=""
-              plus4=""
-              text4=""
-            />
-
-      </div>
-    </div>
-    <div className="ss-right">
-
-      <h2 data-aos="fade-left" data-aos-delay="200" data-aos-duration="1000">
-        {data.Marathwada_coach_factory_heading}
-      </h2>
-      <div className="line" data-aos="fade-left" data-aos-delay="300" data-aos-duration="1000"></div>
-
-      <p data-aos="fade-left" data-aos-delay="400" data-aos-duration="1000">
-        {data.Marathwada_coach_factory_para}
-      </p>
-
-    </div>
-  </div>
-</div>
+            
 
 
 
-<div className="counter-parent1">
+  <div className="counter-parent1">
 <Counter data={props.data}
   background=""
   textnum1="600"
