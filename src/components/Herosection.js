0cd40ebfce9d88ddb1d4c21d1edc041c8c0e3faa
Rishabh Khade <89901588+rishabhkhade@users.noreply.github.com>
2024-06-18 15:47:19 +0530

    HERO SLIDER

--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -13,17 +13,27 @@ function HeroSection(props) {
   const [slide3, setslide3] = useState(false);
 
   useEffect(() => {
+    const toggleSlides = () => {
+      setslide1((prev) => !prev);
+      setslide2((prev) => !prev);
+      setslide3((prev) => !prev);
+    };
+
     const interval = setInterval(() => {
       if (slide1) {
         setslide1(false);
         setslide2(true);
-      } else if (slide2) {
-        setslide2(false);
-        setslide1(true);
-      } else {
         setslide3(false);
-        setslide2(true);
+      } else if (slide2) {
+        setslide1(false);
+        setslide2(false);
+        setslide3(true);
+      } else {
+        setslide1(true);
+        setslide2(false);
+        setslide3(false);
       }
+      return;
     }, 6000);
 
     return () => clearInterval(interval);
@@ -72,7 +82,7 @@ function HeroSection(props) {
                   {slide3 && (
                     <SwiperSlide>
                       <div className="slide3 slide-img">
-                        <h1>{data.Home_slider1}</h1>
+                        <h1>{data.Home_slider2}</h1>
                         <div className="overlay"></div>
                         <div className="spn-frame bg-img-cover"></div>
                         <div className="slide-main bg-img-contain"></div>
